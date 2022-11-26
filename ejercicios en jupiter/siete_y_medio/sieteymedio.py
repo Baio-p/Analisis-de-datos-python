502f1280cefeb1cdf70f2dd488cc9c9c132cd81d
esc_codigo6 <esc_codigo6@test.com>
2022-11-26 14:45:23 -0600

    cambios a ejercicios

---
+++ ejercicios en jupiter/siete_y_medio/sieteymedio.py
@@ -0,0 +1,80 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "juego siete y medio\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "baraja = [\n",
+    "    (\"As Espadas\",1)\n",
+    "    (\"2 Espadas\",2)\n",
+    "    (\"3 Espadas\",3)\n",
+    "    (\"4 Espadas\",4)\n",
+    "    (\"5 Espadas\",5)\n",
+    "    (\"6 Espadas\",6)\n",
+    "    (\"7 Espadas\",7)\n",
+    "    (\"10 Espadas\",0.5)\n",
+    "    (\"11 Espadas\",0.5)\n",
+    "    (\"12 Espadas\",0.5)\n",
+    "    (\"As Oros\",1)\n",
+    "    (\"2 Oros\",2)\n",
+    "    (\"3 Oros\",3)\n",
+    "    (\"4 Oros\",4)\n",
+    "    (\"5 Oros\",5)\n",
+    "    (\"6 Oros\",6)\n",
+    "    (\"7 Oros\",7)\n",
+    "    (\"10 Oros\",0.5)\n",
+    "    (\"11 Oros\",0.5)\n",
+    "    (\"12 Oros\",0.5)\n",
+    "    (\"As Bastos\",1)\n",
+    "    (\"2 Bastos\",2)\n",
+    "    (\"3 Bastos\",3)\n",
+    "    (\"4 Bastos\",4)\n",
+    "    (\"5 Bastos\",5)\n",
+    "    (\"6 Bastos\",6)\n",
+    "    (\"7 Bastos\",7)\n",
+    "    (\"10 Bastos\",0.5)\n",
+    "    (\"11 Bastos\",0.5)\n",
+    "    (\"12 Bastos\",0.5)\n",
+    "    (\"As Copas\",1)\n",
+    "    (\"2 Copas\",2)\n",
+    "    (\"3 Copas\",3)\n",
+    "    (\"4 Copas\",4)\n",
+    "    (\"5 Copas\",5)\n",
+    "    (\"6 Copas\",6)\n",
+    "    (\"7 Copas\",7)\n",
+    "    (\"10 Copas\",0.5)\n",
+    "    (\"11 Copas\",0.5)\n",
+    "    (\"12 Copas\",0.5)\n",
+    "    ]"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.11.0 64-bit",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.11.0"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "0b0b049f9f07c796a5db9b973cc3ef1a7f890e99374bb13bd3a985708b2ada45"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
